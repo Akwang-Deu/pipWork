5d8d5d66fcb3dc3961f55e36b37c752591d1e6c7
Akwang-Deu <akwangdeu@gmail.com>
2024-05-09 23:12:15 +0300

    add files

---
+++ function.py
@@ -0,0 +1,11 @@
+def store_followers(user_name,followers):
+        list_of_followes=[]
+        id=lens(list_of_followes)+1
+        userdetails={username:username,followers:followers}
+        userdetail=list_of_followes.append(userdetails)
+        return userdetail
+
+
+
+
+                                
